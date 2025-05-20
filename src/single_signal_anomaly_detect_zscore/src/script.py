31aea602cc241956d82d7116ecd9dd92e5ce63a1
root <t.frydek@gmx.com>
2025-05-20 10:05:29 +0200

    single_signal_analytics cleanup

--- src/single_signal_anomaly_detect_zscore/src/script.py
+++ src/single_signal_anomaly_detect_zscore/src/script.py
@@ -49,20 +49,22 @@ def get_env_var(
     allow_convert = [str, int, float]
     if req_type not in allow_convert and req_type is not None:
         logger.error(
-            f"Cannot convert value of env_var {env_var} to {req_type}. \
-                Allowed convert type: str, int, float"
+            "Cannot convert value of env_var %s to %s. \
+                Allowed convert type: str, int, float",
+                env_var, req_type
         )
         raise SystemExit
 
     # Return value of env variable
     if env_val is None and default is None:
         # env_var does not exist and we did not set default value
-        logger.error(f"Env variable {env_var} does not exist")
+        logger.error("Env variable %s does not exist", env_var)
         raise SystemExit
     elif env_val is None:
         # env_var does not exist but return default (default is different than none)
         logger.warning(
-            f"Env variable {env_var} does not exist, return default value: {default}"
+            "Env variable %s does not exist, return default value: %s",
+            env_var, default
         )
         return default
     elif env_type is not req_type and req_type is not None:
@@ -70,19 +72,21 @@ def get_env_var(
         try:
             converted_env = req_type(env_val)
             logger.info(
-                f"Env variable {env_var} value: {env_val}. Converted from {env_type} to {req_type}."
+                "Env variable %s value: %s. Converted from %s to %s.",
+                env_var, env_val, env_type, req_type
             )
             return converted_env
         except Exception as e:
             logger.error(
-                f"Convert env_var variable {env_var} from {env_type} to {req_type} failed: {e}"
+                "Convert env_var variable %s from %s to %s failed: %s",
+                env_var, env_type, req_type
             )
             raise SystemExit
     else:
         # env_var exists, is the same type (or we not set type)
-        logger.info(f"Env variable {env_var} value: {env_val}, type: {env_type}")
+        logger.info("Env variable %s value: %s, type: %s",
+                    env_var, env_val, env_type)
         return env_val
-
 
 
 logger.info("Setting const global variables")
@@ -105,7 +109,7 @@ INFLUX_JITTER_INTERVAL = get_env_var("INFLUX_JITTER_INTERVAL", int)
 INFLUX_ORG = get_env_var("INFLUX_ORG", str)
 INFLUX_TOKEN = get_env_var("INFLUX_TOKEN", str)
 INFLUX_URL = "http://" + INFLUX_HOST + ":" + INFLUX_PORT
-logger.info(f"INFLUX_URL value is:  {INFLUX_URL} ")
+logger.info("INFLUX_URL value is:  %s", INFLUX_URL)
 
 # Threshold for z-score value. Point above this threshold is treated as anomaly
 
@@ -230,7 +234,8 @@ class AnomalyDetectionZscore:
                     self._anomaly_ratio = round(sum(self._anomaly_list) / self._anomaly_list_size, 3)
         except Exception as e:
             logger.error(
-                f"Calculation `anomaly ratio of model` {self._name} failed. Error code/reason: {e}"
+                "Calculation `anomaly ratio of model` %s failed. Error code/reason: %s",
+                self._name, e
             )
 
     def check_if_anomaly(self, value: float):
@@ -269,7 +274,8 @@ class AnomalyDetectionZscore:
 
         except Exception as e:
             logger.error(
-                f'Calculation `anomaly of model` "{self._name}" failed. Error code/reason: {e}'
+                'Calculation `anomaly of model` %s failed. Error code/reason: %s',
+                self._name, e
             )
 
 class OpcHandlerAnalytics:
@@ -303,7 +309,7 @@ class OpcHandlerAnalytics:
             except Exception as e:
                 logging.error(f"Unable to connect ot OPC server {e}")
                 logging.error(
-                    f"Trying to re-establish connection with OPC server in 3 seconds "
+                    "Trying to re-establish connection with OPC server in 3 seconds "
                 )
                 time.sleep(3)
 
@@ -320,7 +326,7 @@ class OpcHandlerAnalytics:
             if self.client:
                 self.node_id = node_id
                 self.node = self.client.get_node(node_id)
-                logging.info(f"Configured node: {self.node}")
+                logging.info("Configured node: %s", self.node)
             else:
                 logging.error("OPC client is not created!")
         else:
@@ -347,7 +353,8 @@ class OpcHandlerAnalytics:
                     sensor_value  = opc_node_children[3].get_value()
                     timestamp     = opc_node_children[4].get_value()
                 except Exception as e:
-                    logging.error(f"Error reading node {self.node} || {opc_node_children}: {e}")
+                    logging.error("Error reading node %s || %s: %s",
+                                  self.node, opc_node_children, e)
                     continue
                 else:
 
@@ -413,7 +420,7 @@ class OpcHandlerAnalytics:
                 write_api.write(INFLUX_BUCKET_NAME, INFLUX_ORG, point)
 
         except Exception as e:
-            logging.error(f"Send data to InfluxDB failed. Error code/reason: {e}")
+            logging.error("Send data to InfluxDB failed. Error code/reason: %s", e)
 
 
 # Main function of script
@@ -434,7 +441,7 @@ if __name__ == "__main__":
                                     retry_interval=1000)
 
     except Exception as e:
-        logger.error(f"Configuring InfluxDB failed. Error code/reason: {e}")
+        logger.error("Configuring InfluxDB failed. Error code/reason: %s", e)
 
 
     # Configure connection with OPC UA server
